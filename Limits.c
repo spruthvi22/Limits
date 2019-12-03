de8964d8f5a2b98d53b0c0ea818539a3f5572f92
pruthvi <s.pruthvi22@gmail.com>
2019-12-03 05:30:14 +0000

    Minor correction to initial code

--- Limits.c
+++ Limits.c
@@ -14,9 +14,9 @@ int main()
   printf("Min of int is %f \n",pow(2,8*sizeof(int)-1)*-1);
   printf("Max of long int is %f \n",pow(2,8*sizeof(long int)-1)-1);
   printf("Min of long int is %f \n",pow(2,8*sizeof(long int)-1)*-1);
-  printf("Max of float is %lE \n",(pow(2,24-1)-1)*pow(10,255));
-  printf("Min of float is %lE \n",pow(2,24-1)*pow(10,255)*-1);
-  printf("Max of double is %lE \n",(pow(2,53-1)-1)*pow(10,255));
-  printf("Min of double is %lE \n",pow(2,53-1)*pow(10,255)*-1);
+  printf("Max of float is %lE \n",(pow(2,24-1)-1)*pow(10,127));
+  printf("Min of float is %lE \n",pow(2,24-1)*pow(10,127)*-1);
+  printf("Max of double is %LE \n",(pow(2,53-1)-1)*powl(10,1023));
+  printf("Min of double is %LE \n",pow(2,53-1)*powl(10,1023)*-1);
   return 0;
 }
